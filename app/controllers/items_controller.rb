7b8e7af634d3956150bc3387949b00219cb024c3
funamoto-san <naokifunamoto24@gmail.com>
2020-08-20 16:24:46 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,6 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.valid?
-      binding.pry
       @item.save!
       redirect_to root_path
     else
