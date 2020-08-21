497c7d1dc253ab7ec13fdbe8ca8644f0fb60aa25
funamoto-san <naokifunamoto24@gmail.com>
2020-08-21 14:53:51 +0900

    商品詳細表示機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,3 @@ Rails.application.routes.draw do
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# render :new
-# ルーティングを経由しない
-# →エラーメッセージが表示される
-
-# redirect_to（りろーどぼたん）
-# →エラーメッセージが表示されない
